7cd7e8fa6f900878db85bf20908fa625c39e4cc4
Callum Tolley <cgtrolley@gmail.com>
2016-11-07 13:23:39 +0000

    Added images for README.md

--- matlab/kmeans_example.m
+++ matlab/kmeans_example.m
@@ -1,7 +1,41 @@
 load fisheriris;
 data = meas(:,3:4);
+n = size(data);
+n = n(1);
+x = data(:,1);
+y = data(:,2);
+
+% Render the example data
 figure
-scatter(data(:,1),data(:,2),10,'red','filled');
+scatter(x,y,10,'red','filled');
 xlabel('X');
 ylabel('Y');
 
+k = 2;
+% Get the starting cluster points by choosing k random numbers
+%{
+clusters = double.empty(0,2);
+i = 1;
+while i <= k
+    r = randi(n);
+    if any(data(r,:) == clusters)
+        % We don't want two clusters to be the same, choose another random
+        % number
+        continue
+    end
+    clusters(i,:) = data(r,:);
+    i = i + 1;
+end
+%}
+
+% Use this for now
+clusters = [6.3 1.8;4.0 1.2];
+
+% Make a new figure with the data, as well as the cluster points
+figure
+hold on
+scatter(x,y,10,'red','filled');
+scatter(clusters(:,1), clusters(:,2), 100, 'x','blue');
+hold off
+xlabel('X');
+ylabel('Y');
